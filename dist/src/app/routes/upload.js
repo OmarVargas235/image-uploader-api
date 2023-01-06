821307846443d1f84d6ef51438c707f31a49c4fd
OmarVargas235 <ov1356272@gmail.com>
2023-01-06 17:47:44 -0500

    update

--- dist/src/app/routes/upload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const upload_1 = require("../controllers/upload");
-const router = (0, express_1.Router)();
-router.post('/upload-img', upload_1.uploadImg);
-module.exports = router;
